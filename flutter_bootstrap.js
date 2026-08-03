b3852b8085b2cab5f5613d88c630b8d559846404
sherjintintu <77336410+sherjintintu@users.noreply.github.com>
2026-08-03 19:52:33 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "151158002" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3210218434" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
